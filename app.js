db499819eb360021a59f0d6119cfc54d38e89a33
Sol Nguyen <solnguyen93@gmail.com>
2024-04-10 15:01:31 -0700

    added / page to backend

--- app.js
+++ app.js
@@ -1,47 +1,50 @@
-"use strict";
+'use strict';
 
 /** Express app for jobly. */
 
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
-const { NotFoundError } = require("./expressError");
+const { NotFoundError } = require('./expressError');
 
-const { authenticateJWT } = require("./middleware/auth");
-const authRoutes = require("./routes/auth");
-const companiesRoutes = require("./routes/companies");
-const usersRoutes = require("./routes/users");
-const jobsRoutes = require("./routes/jobs");
+const { authenticateJWT } = require('./middleware/auth');
+const authRoutes = require('./routes/auth');
+const companiesRoutes = require('./routes/companies');
+const usersRoutes = require('./routes/users');
+const jobsRoutes = require('./routes/jobs');
 
-const morgan = require("morgan");
+const morgan = require('morgan');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(morgan('tiny'));
 app.use(authenticateJWT);
 
-app.use("/auth", authRoutes);
-app.use("/companies", companiesRoutes);
-app.use("/users", usersRoutes);
-app.use("/jobs", jobsRoutes);
+app.use('/auth', authRoutes);
+app.use('/companies', companiesRoutes);
+app.use('/users', usersRoutes);
+app.use('/jobs', jobsRoutes);
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the jobly backend!');
+});
 
 /** Handle 404 errors -- this matches everything */
 app.use(function (req, res, next) {
-  return next(new NotFoundError());
+    return next(new NotFoundError());
 });
 
 /** Generic error handler; anything unhandled goes here. */
 app.use(function (err, req, res, next) {
-  if (process.env.NODE_ENV !== "test") console.error(err.stack);
-  const status = err.status || 500;
-  const message = err.message;
+    if (process.env.NODE_ENV !== 'test') console.error(err.stack);
+    const status = err.status || 500;
+    const message = err.message;
 
-  return res.status(status).json({
-    error: { message, status },
-  });
+    return res.status(status).json({
+        error: { message, status },
+    });
 });
 
 module.exports = app;
